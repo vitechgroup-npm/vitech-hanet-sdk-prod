fad07fa59fb90c692bea8912a8f7c0aa612837a8
htqxd <hungtrinhnd91@gmail.com>
2026-05-28 17:16:01 +0700

    1.0.0

--- openapi-ts.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from '@hey-api/openapi-ts';
-
-export default defineConfig({
-  input: './scripts/openapi.json',
-  output: 'src/sdk',
-  client: '@hey-api/client-fetch'
-});
